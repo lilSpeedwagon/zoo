8a246135b8b11796a6cee7a16dd0589ebc52d395
Speedwagon <ded_eg0r@mail.ru>
2022-04-10 16:41:07 +0300

    add thirdparty json lib

---
+++ libraries/common/include/json.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <thirdparty/nlohmann_json/json.hpp>
+
+namespace common {
+
+// nlohmann json library under the MIT license 
+// JSON for Modern C++ version 3.10.5
+// Repository: https://github.com/nlohmann/json
+// Docs: https://json.nlohmann.me/
+namespace json = nlohmann;
+
+} // namespace common
