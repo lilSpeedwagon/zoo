2cecf60b46db4c2defd489eb2c5afba72f130cea
lilSpeedwagon <ded_eg0r@mail.ru>
2022-04-30 15:07:54 +0300

    use linux lib c functions

--- libraries/common/src/utils/errors.cpp
+++ libraries/common/src/utils/errors.cpp
@@ -1,11 +1,20 @@
 #include <common/include/utils/errors.hpp>
 
+#include <errno.h>
+#include <string.h>
+
 namespace common::utils::errors {
 
 std::string GetLastError() {
     constexpr const size_t kBufferSize = 256;
     char buffer[kBufferSize];
+    // In MSVS strerror_s() has a reversed params order.
+    // In other compilers it returns resultin buffer ptr.
+    #ifdef _MSC_VER
     strerror_s(buffer, kBufferSize, errno);
+    #else
+    strerror_r(errno, buffer, kBufferSize);
+    #endif
     return std::string(buffer);
 }
 
