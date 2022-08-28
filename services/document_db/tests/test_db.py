be8c606d045507252729a73e8d45265de529c8a2
lilSpeedwagon <ded_eg0r@mail.ru>
2022-08-28 18:22:09 +0400

    second set of tests

--- services/document_db/tests/test_db.py
+++ services/document_db/tests/test_db.py
@@ -132,15 +132,135 @@ def test_get_bad_request(document_db):
     assert(response.status_code == 400)
     assert(response.text == 'Parameter \'id\' not found')
 
-# TODO
-# list
-# list empty
-# update
-# update partial
-# update payload
-# update missing
-# update bad
-# delete
-# delete missing
-# delete bad
-# delete twice
+
+def test_list_empty(document_db):
+    response = document_db.get(f'/api/v1/documents/list')
+    assert(response.status_code == 200)
+    assert(response.json() == {'items': []})
+
+
+def test_list(document_db):
+    documents = [
+        _create_document(document_db, 'name1', 'owner1', 'namespace1', 'payload1'),
+        _create_document(document_db, 'name2', 'owner2', 'namespace2', 'payload2'),
+        _create_document(document_db, 'name3', 'owner3', 'namespace3', 'payload3'),
+    ]
+    
+    response = document_db.get(f'/api/v1/documents/list')
+    assert(response.status_code == 200)
+    assert(response.json() == {'items': documents})
+
+
+def test_update(document_db):
+    document = _create_document(document_db)
+    response = document_db.post(f'/api/v1/documents/update', body={
+        'id': document['id'],
+        'name': 'new name',
+        'namespace': 'new namespace',
+    })
+    assert(response.status_code == 200)
+    result = response.json()
+    assert(result == {
+        'id': document['id'],
+        'created': document['created'],
+        'updated': MockAny(),
+        'name': 'new name',
+        'owner': document['owner'],
+        'namespace': 'new namespace',
+    })
+
+
+def test_update_payload(document_db):
+    document = _create_document(document_db)
+    response = document_db.post(f'/api/v1/documents/update', body={
+        'id': document['id'],
+        'payload': 'new payload',
+    })
+    assert(response.status_code == 200)
+    result = response.json()
+    assert(result == {
+        'id': document['id'],
+        'created': document['created'],
+        'updated': MockAny(),
+        'name': document['name'],
+        'owner': document['owner'],
+        'namespace': document['namespace'],
+    })
+
+    response = document_db.get(f'/api/v1/documents/get?id={document["id"]}')
+    assert(response.status_code == 200)
+    assert(response.json()['payload'] == 'new payload')
+
+
+def test_update_missing(document_db):
+    response = document_db.post(f'/api/v1/documents/update', 
+                                body={'id': 0, 'name': 'new_name'})
+    assert(response.status_code == 404)
+    assert(response.text == 'Document with id=\'0\' not found')
+
+
+def test_update_missing_id(document_db):
+    response = document_db.post(f'/api/v1/documents/update',
+                                body={'name': 'new name'})
+    assert(response.status_code == 400)
+    assert(response.text == 'Key \'id\' is required.')
+
+
+def test_update_nothing_to_update(document_db):
+    document = _create_document(document_db)
+    response = document_db.post(f'/api/v1/documents/update',
+                                body={'id': document['id']})
+    assert(response.status_code == 200)
+    assert(response.json() == document)
+
+
+def test_update_extra_key(document_db):
+    document = _create_document(document_db)
+    response = document_db.post(f'/api/v1/documents/update', body={
+        'id': document['id'],
+        'name': 'new name',
+        'extra_key': 'value'
+    })
+    assert(response.status_code == 200)
+    assert(response.json() == {
+        'id': document['id'],
+        'created': MockAny(),
+        'updated': MockAny(),
+        'name': 'new name',
+        'owner': document['owner'],
+        'namespace': document['namespace'],
+    })
+
+
+def test_delete(document_db):
+    document = _create_document(document_db)
+    response = document_db.post(f'/api/v1/documents/delete',
+                                body={'id': document['id']})
+    assert(response.status_code == 200)
+    assert(response.json() == document)
+
+
+def test_delete_missing(document_db):
+    response = document_db.post(f'/api/v1/documents/delete',
+                                body={'id': 0})
+    assert(response.status_code == 404)
+    assert(response.text == 'Document with id=\'0\' not found')
+
+
+def test_delete_missing_id(document_db):
+    response = document_db.post(f'/api/v1/documents/delete', body={})
+    assert(response.status_code == 400)
+    assert(response.text == 'Key \'id\' is required.')
+
+
+def test_delete_twice(document_db):
+    document = _create_document(document_db)
+    response = document_db.post(f'/api/v1/documents/delete',
+                                body={'id': document['id']})
+    assert(response.status_code == 200)
+    assert(response.json() == document)
+    
+    response = document_db.post(f'/api/v1/documents/delete',
+                                body={'id': document['id']})
+    assert(response.status_code == 404)
+    assert(response.text == f'Document with id=\'{document["id"]}\' not found')
