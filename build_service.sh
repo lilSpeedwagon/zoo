de4846d57d330220893cf47bc553d47bcc45b454
lilSpeedwagon <ded_eg0r@mail.ru>
2023-05-07 22:26:43 +0400

    add build libs step for scripts

--- build_service.sh
+++ build_service.sh
@@ -6,5 +6,13 @@ then
     exit 1
 fi
 
+./build_libraries.sh
+libs_build_result=$?
+if (test ${libs_build_result} != 0)
+then
+    echo 'Build libs failed'
+    exit ${libs_build_result}
+fi
+
 docker build -f ./services/${service}/Dockerfile -t ${service}_service .
 exit $?
