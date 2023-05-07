fe7e6974304905dd9062deaa75c7c2ce7dc95ab6
lilSpeedwagon <ded_eg0r@mail.ru>
2023-05-07 22:26:23 +0400

    add base factory fixture

--- utils/test_utils/fixtures.py
+++ utils/test_utils/fixtures.py
@@ -1,16 +1,22 @@
-from typing import Optional
+from abc import ABC, abstractmethod
+from typing import Any, Optional
 import requests
 
 
-class ServiceBase:
+DEFAULT_HOST = 'localhost'
+DEFAULT_PORT = 80
+DEFAULT_TEST_PORT = 5050
+
+
+class ServiceBase(ABC):
     """
     Base class for service access fixtures. Override host/port in derived class.
     """
 
-    def __init__(self):
-        self.host = 'localhost'
-        self.port = 5555
-        self.test_port = 5050
+    def __init__(self, host = DEFAULT_HOST, port = DEFAULT_PORT, test_port = DEFAULT_TEST_PORT):
+        self.host = host
+        self.port = port
+        self.test_port = test_port
         self.reset()
 
     @property
@@ -31,9 +37,24 @@ class ServiceBase:
         return requests.post(self.make_uri(self.address, path), json=body)
 
     def reset(self, component_name: Optional[str] = None):
-        """"""
+        """
+        Reset service components system state. If component name is specified - clears the component state.
+        """
 
         body = {}
         if component_name:
             body['component_name'] = component_name
         return requests.post(self.make_uri(self.test_address, '/test-control/reset'), json=body)
+
+
+class FactoryBase(ABC):
+    """
+    Base factory class.
+    """
+
+    def __init__(self, service: ServiceBase) -> None:
+        self._service = service
+
+    @abstractmethod
+    def create(self) -> Any:
+        pass
--- utils/test_utils/mocks.py
+++ utils/test_utils/mocks.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 
 class MockAny:
     # Returns True on assertion with any other object.
@@ -11,3 +12,38 @@ class MockAnyString:
     # Returns True on comparison with any other str object.
     def __eq__(self, other):
         return isinstance(other, str)
+
+
+class DateTimeMock:
+    """
+    Mock allows you compare datetime objects (or ISO-formatted strings) with specified accuracy.
+    Use it when you want to check whether a datetime object is differs
+    from the target value not more than on the specified timedelta.
+    """
+
+    def __init__(self, datetime_: datetime, accuracy: timedelta = timedelta(minutes=1)) -> None:
+        self.datetime_ = datetime_
+        self.accuracy = accuracy
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, datetime):
+            return self.datetime_ - self.accuracy <= other <= self.datetime_ + self.accuracy
+        if isinstance(other, str):
+            from dateutil.parser import parse
+            try:
+                dt = parse(other, ignoretz=True)
+            except (TypeError, ValueError):
+                return False
+            return self.__eq__(dt)
+        return False
+
+    def __repr__(self) -> str:
+        return f'<DateTimeMock datetime={self.datetime_} accuracy={self.accuracy}>'
+
+    @staticmethod
+    def from_now(accuracy: timedelta = timedelta(minutes=1)):
+        return DateTimeMock(datetime_=datetime.now(), accuracy=accuracy)
+
+    @staticmethod
+    def from_utc_now(accuracy: timedelta = timedelta(minutes=1)):
+        return DateTimeMock(datetime_=datetime.utcnow(), accuracy=accuracy)
